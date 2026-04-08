689f2a8a3de936521de2e0df02cbe9391d9c1f61
DMQUCS25017052 <DMQUCS25017052@L04P18.digilians.ema>
2026-04-08 10:33:53 +0200

    Add Tea to task list

--- init.js
+++ init.js
@@ -3,5 +3,6 @@ db = db.getSiblingDB('tasksdb');
 db.tasks.insertMany([
   { id: 1, name: "Study", status: "pending" },
   { id: 2, name: "Workout", status: "done" },
-  { id: 3, name: "Shopping", status: "pending" }
+  { id: 3, name: "Shopping", status: "pending" },
+  { id: 7, name: 'Tea', status: 'pending' }
 ]);
